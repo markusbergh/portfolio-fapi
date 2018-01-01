6c5e4d6d61b3f60fa2d9a00f424b795c10b3cc4f
Markus Bergh <hi@markusbergh.se>
2018-01-01 21:39:47 +0100

    Fixed routes for project and model

--- middleware/models.py
+++ middleware/models.py
@@ -4,21 +4,22 @@ class Article():
     """This class represents an article."""
 
     id = ''
-    title = ''
     body = ''
+    category = ''
     cover = ''
     date = ''
-    category = ''
+    title = ''
 
     def __init__(self, id):
         self.id = id
 
     def serialize(self):
         return {
+            'body': self.body,
+            'category': self.category,
+            'cover': self.cover,
             'date': self.date,
             'title': self.title,
-            'body': self.body,
-            'cover': self.cover,
         }
 
 class Project():
@@ -32,14 +33,19 @@ class Project():
     thumbnail = ''
     title = ''
     year = ''
+    screens = ''
 
     def __init__(self, id):
         self.id = id
+
+    def get_screen_url(self, screen):
+        return screen.url()
 
     def serialize(self):
         return {
             'cover': self.cover,
             'description': self.description,
+            'screens': map(self.get_screen_url, self.screens),
             'title': self.title,
             'year': self.year,
         }
--- middleware/views.py
+++ middleware/views.py
@@ -6,7 +6,43 @@ from flask import jsonify, abort
 from middleware import app, client
 
 # Models
-from models import Article
+from models import Article, Project
+
+@app.route('/v1/projects/<string:project_id>', methods=['GET'])
+def get_project(project_id):
+    project = None
+
+    for item in client.entries({'content_type': 'project', 'sys.id': project_id}):
+        project = Project(id = item.id)
+        project.cover = item.cover.url()
+        project.description = item.description
+        project.screens = item.screens
+        project.title = item.title
+        project.year = item.year
+
+    if project is None:
+        abort(404)
+
+    return jsonify(project.serialize())
+
+@app.route('/v1/projects', methods=['GET'])
+def get_projects():
+    projects = []
+
+    for item in client.entries({'content_type': 'project'}):
+        project = Project(id = item.id)
+        project.cover = item.cover.url()
+        project.description = item.description
+        project.screens = item.screens
+        project.title = item.title
+        project.year = item.year
+
+        projects.append(project)
+
+    if len(projects) == 0:
+        abort(404)
+
+    return jsonify([project.serialize() for project in projects])
 
 @app.route('/v1/articles/<string:article_id>', methods=['GET'])
 def get_article(article_id):
@@ -14,10 +50,11 @@ def get_article(article_id):
 
     for item in client.entries({'content_type': 'article', 'sys.id': article_id}):
         article = Article(id = item.id)
-        article.date = item.date
-        article.cover = item.cover.url()
-        article.title = item.title
         article.body = item.body
+        article.category = item.category
+        article.cover = item.cover.url()
+        article.date = item.date
+        article.title = item.title
 
     if article is None:
         abort(404)
@@ -30,10 +67,11 @@ def get_articles():
 
     for item in client.entries({'content_type': 'article'}):
         article = Article(id = item.id)
-        article.date = item.date
-        article.cover = item.cover.url()
-        article.title = item.title
         article.body = item.body
+        article.category = item.category
+        article.cover = item.cover.url()
+        article.date = item.date
+        article.title = item.title
 
         articles.append(article)
 
